78243c748bb9db6894f1bd8a3f1067c9ad9332d0
YevheniiaZalevska <yevhenia05@gmail.com>
2024-03-18 12:54:22 +0100

    45

--- js/task-1.js
+++ js/task-1.js
@@ -1,35 +1,3 @@
-// function makeTransaction(quantity, pricePerDroid) {
-//     const totalCost = quantity * pricePerDroid;
-    
-//     return `You ordered ${quantity} droids worth ${totalCost} credits!`;
-// }
-
-
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  const totalCost = quantity * pricePerDroid;
-
-  if (totalCost > customerCredits) {
-    return "Insufficient funds!";
-  } else {
-    return `You ordered ${quantity} droids worth ${totalCost} credits!`;
-  }
-}
-
-// console.log(makeTransaction(5, 3000, 23000));
-// console.log(makeTransaction(3, 1000, 15000));
-// console.log(makeTransaction(10, 5000, 8000));
-// console.log(makeTransaction(8, 2000, 10000));
-// console.log(makeTransaction(10, 500, 5000));
-
-
-function slugify(title) {
-  return title.toLowerCase().split(' ').join('-');
-}
-
-// console.log(slugify("Arrays for begginers"));
-// console.log(slugify("English for developer"));
-// console.log(slugify("Ten secrets of JavaScript"));
-// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
 
 
 function isEnoughCapacity(products, containerSize) {
